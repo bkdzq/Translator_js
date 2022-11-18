cdc46f4080f912bbca802710652ceeb3ae9b91c0
qian ZHANG <53816482+bkdzq@users.noreply.github.com>
2022-11-18 22:36:09 +0100

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,11 @@
+const countries = {
+  
+    "zh": "	Chinese",
+    "de-DE": "German",
+    "en-GB": "English",
+    "es-ES": "Spanish",
+    "it-IT": "Italian",
+    "ja-JP": "Japanese",
+    "la-VA": "Latin",
+    "ru-RU": "Russian",
+}
